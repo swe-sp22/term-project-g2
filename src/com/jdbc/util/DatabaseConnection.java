e13e61d3fea94eed3e9092f3a4e3133a3ae0fe0d
Muhhamed Sallam <m.sallam2622000@gmail.com>
2022-05-09 02:58:42 +0200

    made database connection packages id:6144

---
+++ src/com/jdbc/util/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package com.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static Connection con = null;
+
+    static //Is used to initialize the static data member.
+    {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql:// localhost:3306/org", "root", "root");
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection()
+    {
+        return con;
+    }
+}
